99803a13e64bf597a86a310ff80b3c00d7cf5351
Mdesarrollo <merobless@ufpso.edu.co>
2024-11-15 03:26:11 -0500

    Maicol Se implementaros adelantos en el main-information y unas pequeñas modificaciones en el register de usuario

--- src/app/register/register.component.ts
+++ src/app/register/register.component.ts
@@ -1,13 +1,29 @@
 import { Component } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button'
-
+import {signal} from '@angular/core';
+import {MatFormFieldModule} from '@angular/material/form-field';
+import {MatIconModule} from '@angular/material/icon';
+import {MatInputModule} from '@angular/material/input';
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [MatButtonModule],
+  imports: [MatButtonModule,MatFormFieldModule,MatIconModule,MatInputModule],
   templateUrl: './register.component.html',
-  styleUrl: './register.component.css'
+  styleUrl: './register.component.scss',
+  
 })
-export class RegisterComponent {
+
+
+export class RegisterComponent   {
+  hide = signal(true);
+  clickEvent(event: MouseEvent) {
+    this.hide.set(!this.hide());
+    event.stopPropagation();
+  }
+  
 
 }
+
+
+
+
