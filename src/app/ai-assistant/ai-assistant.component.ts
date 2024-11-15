ebcdc1c65046656798b1338eaf8044e5572dfb97
Angel Boada <aboadasalazar@gmail.com>
2024-11-15 10:33:11 -0500

    creacion carpeta IA

---
+++ src/app/ai-assistant/ai-assistant.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ai-assistant',
+  standalone: true,
+  imports: [],
+  templateUrl: './ai-assistant.component.html',
+  styleUrl: './ai-assistant.component.css'
+})
+export class AIAssistantComponent {
+
+}
