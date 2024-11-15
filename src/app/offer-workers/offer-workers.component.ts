e27d5a3f1ee30c8583c0bc64a2ce025acb312331
JuanDuranGarzon <jddurang@ufpso.edu.co>
2024-11-14 23:32:49 -0500

    Garzon

--- src/app/offer-workers/offer-workers.component.ts
+++ src/app/offer-workers/offer-workers.component.ts
@@ -1,15 +1,30 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { MatButtonModule } from '@angular/material/button';
+import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatCardModule } from '@angular/material/card';
 import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatExpansionModule } from '@angular/material/expansion';
 
 @Component({
   selector: 'app-offer-workers',
   standalone: true,
-  imports: [CommonModule, MatButtonModule, MatIconModule],
+  imports: [CommonModule, 
+    MatButtonModule, 
+    MatIconModule,
+    BrowserModule,
+    BrowserAnimationsModule,
+    MatCardModule,
+    MatExpansionModule
+  ],
   templateUrl: './offer-workers.component.html',
   styleUrl: './offer-workers.component.css'
 })
 export class OfferWorkersComponent {
-
+  items = [
+    { title: 'Información adicional 1', content: 'Contenido del primer panel desplegable' },
+    { title: 'Información adicional 2', content: 'Contenido del segundo panel desplegable' },
+    { title: 'Información adicional 3', content: 'Contenido del tercer panel desplegable' }
+  ];
 }
--- src/app/plan-plus/plan-plus.component.ts
+++ src/app/plan-plus/plan-plus.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule } from '@angular/material/button';
+import { MatInputModule } from '@angular/material/input';
 
 @Component({
   selector: 'app-plan-plus',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, MatButtonModule, MatInputModule],
   templateUrl: './plan-plus.component.html',
   styleUrl: './plan-plus.component.css'
 })
